b4b15645913a93a935d95a21c7df85375f3bef76
chanhihi <chanhihi55@gmail.com>
2022-08-19 19:30:55 +0900

    day02

---
+++ StoryBoard/day02/day02/Resource.swift
@@ -0,0 +1,10 @@
+//
+//  Resource.swift
+//  day02
+//
+//  Created by Chan on 2022/08/18.
+//
+
+var names = ["Donald Pmurt", "Joffrey", "Ben"]
+var date = ["8/18", "7/13", "9/13"]
+var subject = ["no", "em", "tue"]
