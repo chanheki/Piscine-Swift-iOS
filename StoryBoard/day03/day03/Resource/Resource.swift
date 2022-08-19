6d81e629350887d78cfeb6ad55a214124f1be8e1
chanhihi <chanhihi55@gmail.com>
2022-08-19 19:31:07 +0900

    day03

---
+++ StoryBoard/day03/day03/Resource/Resource.swift
@@ -0,0 +1,10 @@
+//
+//  Resource.swift
+//  day03
+//
+//  Created by Chan on 2022/08/19.
+//
+
+public var imagesItem = ["dragonXspace", "m33", "m51", "nauticus"]
+
+public var imagesURL = ["https://www.nasa.gov/sites/default/files/thumbnails/image/nauticus_4.jpg", "https://www.nasa.gov/sites/default/files/thumbnails/image/iss067e189232.jpg", "https://www.nasa.gov/sites/default/files/thumbnails/image/iss067e253797.jpg", "https://www.nasa.gov/sites/default/files/thumbnails/image/sls-4215_-_sls_artemis_i_infographic.jpg"]
